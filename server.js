3edcaf5f59068cf47be74384cbe10cbf1bf1cf05
Nathaniel Ellsworth <NathanielEllsworth@gmail.com>
2017-07-07 06:22:07 -0400

    yesterday's group work

--- server.js
+++ server.js
@@ -1,56 +1,117 @@
 var express = require("express");
 var bodyParser = require("body-parser");
+var moment = require('moment');
+
+
 
 var app = express();
-var port =3000;
+var port = 3000;
+
+// Serve static content for the app from the "public" directory in the application directory.
+app.use(express.static("public"));
+
+// Parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({extended: false}));
+
+var exphbs = require("express-handlebars");
+
+app.engine("handlebars", exphbs({defaultLayout: "main"}));
+app.set("view engine", "handlebars");
 
 var mysql = require("mysql");
 
+moment().format("dddd, MMMM YYYY");
+
+
+
 var connection = mysql.createConnection({
-    host: "localhost",
-    user: "root",
+    host    : "localhost",
+    user    : "root",
     password: "",
     database: "Ethereum"
-
 });
 
-connection.connect(function (err) {
-    if (err){
-        console.log(error)
+connection.connect(function(err) {
+    if (err) {
+        console.error("error connecting: " + err.stack);
+        return;
     }
 
+    console.log("connected as id " + connection.threadId);
 
 });
 
+app.get("/", function(req, res) {
+    connection.query("SELECT etherprice.`price_USD`, `marketcap_value`.`market-cap-value`, transactions.transactions, marketcap_value.timestamp FROM `marketcap_value` INNER JOIN etherprice, transactions WHERE etherprice.timestamp = `marketcap_value`.timestamp AND marketcap_value.timestamp = transactions.timestamp ORDER BY `timestamp` LIMIT 10", function(err, data) {
+        if (err) {
+            throw err;
+        }
+        //console.log(data)
+        //var ts = Date.now()
+        moment({enterprice: data.timestamp}).format("dddd, MMMM YYYY");
+        res.render("index", {etherprice: data});
 
-/**
- *  this is the data string needed to create the table of the needed data
- */
+    });
+});
 
-// SELECT etherprice.`price_USD`, `marketcap_value`.`market-cap-value`, transactions.transactions, marketcap_value.timestamp FROM `marketcap_value` INNER JOIN etherprice, transactions WHERE etherprice.timestamp = `marketcap_value`.timestamp AND marketcap_value.timestamp = transactions.timestamp;
+app.get("/page2", function(req, res) {
+    connection.query("SELECT etherprice.`price_USD`, `marketcap_value`.`market-cap-value`, transactions.transactions, marketcap_value.timestamp FROM `marketcap_value` INNER JOIN etherprice, transactions WHERE etherprice.timestamp = `marketcap_value`.timestamp AND marketcap_value.timestamp = transactions.timestamp order by `timestamp` LIMIT 10 offset 10", function(err, data) {
+        if (err) {
+            throw err;
+        }
+        //console.log(data)
+        //var ts = Date.now()
+        moment({enterprice: data.timestamp}).format("dddd, MMMM YYYY");
+        res.render("index", {etherprice: data});
+
+    });
+});
+
+app.post("/", function(req, res) {
+    connection.query("INSERT INTO plans (plan) VALUES (?)", [req.body.plan],
+        function(err, result) {
+            if (err) {
+                throw err;
+            }
+            //TODO what do we do?
+            res.redirect('/');
+        });
+});
+
+app.post("/delete/", function(req, res) {
+    connection.query("DELETE FROM plans WHERE id = ?", [req.body.id],
+        function(err, result) {
+            if (err) {
+                throw err;
+            }
 
 
-/**
- * Look at 08 task-saver 'server.js'
- */
+            //TODO what do we do?
+            res.redirect('/');
+        });
+});
+
+app.post("/update", function(req, res) {
+
+    connection.query("UPDATE plans SET plan = ? WHERE id = ?",
+        [req.body.plan, req.body.id], function(err, result) {
+            if (err) {
+                throw err;
+            }
+
+            res.redirect('/');
+            //TODO what do we do?
+        });
+});
+
+app.listen(port);
 
 
 // var express = require("express");
 // var bodyParser = require("body-parser");
 //
 // var app = express();
-// var port = 3000;
-//
-// // Serve static content for the app from the "public" directory in the application directory.
-// app.use(express.static("public"));
-//
-// // Parse application/x-www-form-urlencoded
-// app.use(bodyParser.urlencoded({ extended: false }));
-//
-// var exphbs = require("express-handlebars");
-//
-// app.engine("handlebars", exphbs({ defaultLayout: "main" }));
-// app.set("view engine", "handlebars");
+// var port =3000;
 //
 // var mysql = require("mysql");
 //
@@ -58,8 +119,38 @@ connection.connect(function (err) {
 //     host: "localhost",
 //     user: "root",
 //     password: "",
-//     database: "task_saver_db"
+//     database: "Ethereum"
+//
 // });
+//
+// connection.connect(function (err) {
+//     if (err){
+//         console.log(error)
+//     }
+//
+//
+// });
+//
+//
+// app.use(express.static())
+
+
+
+
+// /**
+//  *  this is the data string needed to create the table of the needed data
+//  */
+//
+// // SELECT etherprice.`price_USD`, `marketcap_value`.`market-cap-value`, transactions.transactions, marketcap_value.timestamp FROM `marketcap_value` INNER JOIN etherprice, transactions WHERE etherprice.timestamp = `marketcap_value`.timestamp AND marketcap_value.timestamp = transactions.timestamp;
+//
+//
+// /**
+//  * Look at 08 task-saver 'server.js'
+//  */
+//
+//
+//
+//
 //
 // connection.connect(function(err) {
 //     if (err) {
@@ -104,5 +195,3 @@ connection.connect(function (err) {
 //         res.redirect('/') //*****************************
 //     });
 // });
-//
-
