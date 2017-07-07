5a829306ffac9d326b29d1f37c3bac4713f9a5df
Nathaniel Ellsworth <NathanielEllsworth@gmail.com>
2017-07-07 06:53:40 -0400

    working UI data paging buttons

--- server.js
+++ server.js
@@ -41,8 +41,10 @@ connection.connect(function(err) {
 
 });
 
+
+//page 1
 app.get("/", function(req, res) {
-    connection.query("SELECT etherprice.`price_USD`, `marketcap_value`.`market-cap-value`, transactions.transactions, marketcap_value.timestamp FROM `marketcap_value` INNER JOIN etherprice, transactions WHERE etherprice.timestamp = `marketcap_value`.timestamp AND marketcap_value.timestamp = transactions.timestamp ORDER BY `timestamp` LIMIT 10", function(err, data) {
+    connection.query("SELECT etherprice.`price_USD`, `marketcap_value`.`market-cap-value`, transactions.transactions, marketcap_value.timestamp FROM `marketcap_value` INNER JOIN etherprice, transactions WHERE etherprice.timestamp = `marketcap_value`.timestamp AND marketcap_value.timestamp = transactions.timestamp ORDER BY `timestamp` LIMIT 38", function(err, data) {
         if (err) {
             throw err;
         }
@@ -54,8 +56,253 @@ app.get("/", function(req, res) {
     });
 });
 
+
+//page 2
 app.get("/page2", function(req, res) {
-    connection.query("SELECT etherprice.`price_USD`, `marketcap_value`.`market-cap-value`, transactions.transactions, marketcap_value.timestamp FROM `marketcap_value` INNER JOIN etherprice, transactions WHERE etherprice.timestamp = `marketcap_value`.timestamp AND marketcap_value.timestamp = transactions.timestamp order by `timestamp` LIMIT 10 offset 10", function(err, data) {
+    connection.query("SELECT etherprice.`price_USD`, `marketcap_value`.`market-cap-value`, transactions.transactions, marketcap_value.timestamp FROM `marketcap_value` INNER JOIN etherprice, transactions WHERE etherprice.timestamp = `marketcap_value`.timestamp AND marketcap_value.timestamp = transactions.timestamp order by `timestamp` LIMIT 38 offset 38", function(err, data) {
+        if (err) {
+            throw err;
+        }
+        //console.log(data)
+        //var ts = Date.now()
+        moment({enterprice: data.timestamp}).format("dddd, MMMM YYYY");
+        res.render("index", {etherprice: data});
+
+    });
+});
+
+
+//page3
+app.get("/page3", function(req, res) {
+    connection.query("SELECT etherprice.`price_USD`, `marketcap_value`.`market-cap-value`, transactions.transactions, marketcap_value.timestamp FROM `marketcap_value` INNER JOIN etherprice, transactions WHERE etherprice.timestamp = `marketcap_value`.timestamp AND marketcap_value.timestamp = transactions.timestamp order by `timestamp` LIMIT 38 offset 76", function(err, data) {
+        if (err) {
+            throw err;
+        }
+        //console.log(data)
+        //var ts = Date.now()
+        moment({enterprice: data.timestamp}).format("dddd, MMMM YYYY");
+        res.render("index", {etherprice: data});
+
+    });
+});
+
+//page 4
+app.get("/page4", function(req, res) {
+    connection.query("SELECT etherprice.`price_USD`, `marketcap_value`.`market-cap-value`, transactions.transactions, marketcap_value.timestamp FROM `marketcap_value` INNER JOIN etherprice, transactions WHERE etherprice.timestamp = `marketcap_value`.timestamp AND marketcap_value.timestamp = transactions.timestamp order by `timestamp` LIMIT 38 offset 114", function(err, data) {
+        if (err) {
+            throw err;
+        }
+        //console.log(data)
+        //var ts = Date.now()
+        moment({enterprice: data.timestamp}).format("dddd, MMMM YYYY");
+        res.render("index", {etherprice: data});
+
+    });
+});
+
+
+//page 5
+app.get("/page5", function(req, res) {
+    connection.query("SELECT etherprice.`price_USD`, `marketcap_value`.`market-cap-value`, transactions.transactions, marketcap_value.timestamp FROM `marketcap_value` INNER JOIN etherprice, transactions WHERE etherprice.timestamp = `marketcap_value`.timestamp AND marketcap_value.timestamp = transactions.timestamp order by `timestamp` LIMIT 38 offset 152", function(err, data) {
+        if (err) {
+            throw err;
+        }
+        //console.log(data)
+        //var ts = Date.now()
+        moment({enterprice: data.timestamp}).format("dddd, MMMM YYYY");
+        res.render("index", {etherprice: data});
+
+    });
+});
+
+
+
+//page 6
+app.get("/page6", function(req, res) {
+    connection.query("SELECT etherprice.`price_USD`, `marketcap_value`.`market-cap-value`, transactions.transactions, marketcap_value.timestamp FROM `marketcap_value` INNER JOIN etherprice, transactions WHERE etherprice.timestamp = `marketcap_value`.timestamp AND marketcap_value.timestamp = transactions.timestamp order by `timestamp` LIMIT 38 offset 190", function(err, data) {
+        if (err) {
+            throw err;
+        }
+        //console.log(data)
+        //var ts = Date.now()
+        moment({enterprice: data.timestamp}).format("dddd, MMMM YYYY");
+        res.render("index", {etherprice: data});
+
+    });
+});
+
+
+
+//page 7
+app.get("/page7", function(req, res) {
+    connection.query("SELECT etherprice.`price_USD`, `marketcap_value`.`market-cap-value`, transactions.transactions, marketcap_value.timestamp FROM `marketcap_value` INNER JOIN etherprice, transactions WHERE etherprice.timestamp = `marketcap_value`.timestamp AND marketcap_value.timestamp = transactions.timestamp order by `timestamp` LIMIT 38 offset 228", function(err, data) {
+        if (err) {
+            throw err;
+        }
+        //console.log(data)
+        //var ts = Date.now()
+        moment({enterprice: data.timestamp}).format("dddd, MMMM YYYY");
+        res.render("index", {etherprice: data});
+
+    });
+});
+
+
+
+//page 8
+app.get("/page8", function(req, res) {
+    connection.query("SELECT etherprice.`price_USD`, `marketcap_value`.`market-cap-value`, transactions.transactions, marketcap_value.timestamp FROM `marketcap_value` INNER JOIN etherprice, transactions WHERE etherprice.timestamp = `marketcap_value`.timestamp AND marketcap_value.timestamp = transactions.timestamp order by `timestamp` LIMIT 38 offset 266", function(err, data) {
+        if (err) {
+            throw err;
+        }
+        //console.log(data)
+        //var ts = Date.now()
+        moment({enterprice: data.timestamp}).format("dddd, MMMM YYYY");
+        res.render("index", {etherprice: data});
+
+    });
+});
+
+
+
+//page 9
+app.get("/page9", function(req, res) {
+    connection.query("SELECT etherprice.`price_USD`, `marketcap_value`.`market-cap-value`, transactions.transactions, marketcap_value.timestamp FROM `marketcap_value` INNER JOIN etherprice, transactions WHERE etherprice.timestamp = `marketcap_value`.timestamp AND marketcap_value.timestamp = transactions.timestamp order by `timestamp` LIMIT 38 offset 304", function(err, data) {
+        if (err) {
+            throw err;
+        }
+        //console.log(data)
+        //var ts = Date.now()
+        moment({enterprice: data.timestamp}).format("dddd, MMMM YYYY");
+        res.render("index", {etherprice: data});
+
+    });
+});
+
+
+
+//page 10
+app.get("/page10", function(req, res) {
+    connection.query("SELECT etherprice.`price_USD`, `marketcap_value`.`market-cap-value`, transactions.transactions, marketcap_value.timestamp FROM `marketcap_value` INNER JOIN etherprice, transactions WHERE etherprice.timestamp = `marketcap_value`.timestamp AND marketcap_value.timestamp = transactions.timestamp order by `timestamp` LIMIT 38 offset 342", function(err, data) {
+        if (err) {
+            throw err;
+        }
+        //console.log(data)
+        //var ts = Date.now()
+        moment({enterprice: data.timestamp}).format("dddd, MMMM YYYY");
+        res.render("index", {etherprice: data});
+
+    });
+});
+
+
+
+//page 11
+app.get("/page11", function(req, res) {
+    connection.query("SELECT etherprice.`price_USD`, `marketcap_value`.`market-cap-value`, transactions.transactions, marketcap_value.timestamp FROM `marketcap_value` INNER JOIN etherprice, transactions WHERE etherprice.timestamp = `marketcap_value`.timestamp AND marketcap_value.timestamp = transactions.timestamp order by `timestamp` LIMIT 38 offset 380", function(err, data) {
+        if (err) {
+            throw err;
+        }
+        //console.log(data)
+        //var ts = Date.now()
+        moment({enterprice: data.timestamp}).format("dddd, MMMM YYYY");
+        res.render("index", {etherprice: data});
+
+    });
+});
+
+
+
+//page 12
+app.get("/page12", function(req, res) {
+    connection.query("SELECT etherprice.`price_USD`, `marketcap_value`.`market-cap-value`, transactions.transactions, marketcap_value.timestamp FROM `marketcap_value` INNER JOIN etherprice, transactions WHERE etherprice.timestamp = `marketcap_value`.timestamp AND marketcap_value.timestamp = transactions.timestamp order by `timestamp` LIMIT 38 offset 418", function(err, data) {
+        if (err) {
+            throw err;
+        }
+        //console.log(data)
+        //var ts = Date.now()
+        moment({enterprice: data.timestamp}).format("dddd, MMMM YYYY");
+        res.render("index", {etherprice: data});
+
+    });
+});
+
+//page 13
+app.get("/page13", function(req, res) {
+    connection.query("SELECT etherprice.`price_USD`, `marketcap_value`.`market-cap-value`, transactions.transactions, marketcap_value.timestamp FROM `marketcap_value` INNER JOIN etherprice, transactions WHERE etherprice.timestamp = `marketcap_value`.timestamp AND marketcap_value.timestamp = transactions.timestamp order by `timestamp` LIMIT 38 offset 456", function(err, data) {
+        if (err) {
+            throw err;
+        }
+        //console.log(data)
+        //var ts = Date.now()
+        moment({enterprice: data.timestamp}).format("dddd, MMMM YYYY");
+        res.render("index", {etherprice: data});
+
+    });
+});
+
+
+//page 14
+app.get("/page14", function(req, res) {
+    connection.query("SELECT etherprice.`price_USD`, `marketcap_value`.`market-cap-value`, transactions.transactions, marketcap_value.timestamp FROM `marketcap_value` INNER JOIN etherprice, transactions WHERE etherprice.timestamp = `marketcap_value`.timestamp AND marketcap_value.timestamp = transactions.timestamp order by `timestamp` LIMIT 38 offset 494", function(err, data) {
+        if (err) {
+            throw err;
+        }
+        //console.log(data)
+        //var ts = Date.now()
+        moment({enterprice: data.timestamp}).format("dddd, MMMM YYYY");
+        res.render("index", {etherprice: data});
+
+    });
+});
+
+
+//page 15
+app.get("/page15", function(req, res) {
+    connection.query("SELECT etherprice.`price_USD`, `marketcap_value`.`market-cap-value`, transactions.transactions, marketcap_value.timestamp FROM `marketcap_value` INNER JOIN etherprice, transactions WHERE etherprice.timestamp = `marketcap_value`.timestamp AND marketcap_value.timestamp = transactions.timestamp order by `timestamp` LIMIT 38 offset 532", function(err, data) {
+        if (err) {
+            throw err;
+        }
+        //console.log(data)
+        //var ts = Date.now()
+        moment({enterprice: data.timestamp}).format("dddd, MMMM YYYY");
+        res.render("index", {etherprice: data});
+
+    });
+});
+
+//page 16
+app.get("/page16", function(req, res) {
+    connection.query("SELECT etherprice.`price_USD`, `marketcap_value`.`market-cap-value`, transactions.transactions, marketcap_value.timestamp FROM `marketcap_value` INNER JOIN etherprice, transactions WHERE etherprice.timestamp = `marketcap_value`.timestamp AND marketcap_value.timestamp = transactions.timestamp order by `timestamp` LIMIT 38 offset 570", function(err, data) {
+        if (err) {
+            throw err;
+        }
+        //console.log(data)
+        //var ts = Date.now()
+        moment({enterprice: data.timestamp}).format("dddd, MMMM YYYY");
+        res.render("index", {etherprice: data});
+
+    });
+});
+
+
+//page 17
+app.get("/page17", function(req, res) {
+    connection.query("SELECT etherprice.`price_USD`, `marketcap_value`.`market-cap-value`, transactions.transactions, marketcap_value.timestamp FROM `marketcap_value` INNER JOIN etherprice, transactions WHERE etherprice.timestamp = `marketcap_value`.timestamp AND marketcap_value.timestamp = transactions.timestamp order by `timestamp` LIMIT 38 offset 608", function(err, data) {
+        if (err) {
+            throw err;
+        }
+        //console.log(data)
+        //var ts = Date.now()
+        moment({enterprice: data.timestamp}).format("dddd, MMMM YYYY");
+        res.render("index", {etherprice: data});
+
+    });
+});
+
+//page 18
+app.get("/page18", function(req, res) {
+    connection.query("SELECT etherprice.`price_USD`, `marketcap_value`.`market-cap-value`, transactions.transactions, marketcap_value.timestamp FROM `marketcap_value` INNER JOIN etherprice, transactions WHERE etherprice.timestamp = `marketcap_value`.timestamp AND marketcap_value.timestamp = transactions.timestamp order by `timestamp` LIMIT 38 offset 646", function(err, data) {
         if (err) {
             throw err;
         }
